560af583820e4d579d976bfbc2afc97e0482a6fa
sachindra <sachindrapandey@sagoon.com>
2020-06-16 23:26:18 +0800

    Moved image download operation to Operation Queue

---
+++ OMDB/Shared/OMDBImageDownloadOperation.swift
@@ -0,0 +1,13 @@
+//
+//  OMDBImageDownloadOperation.swift
+//  OMDB
+//
+//  Created by Sachindra on 05/06/20.
+//  Copyright © 2020 Sachindra Pandey. All rights reserved.
+//
+
+import UIKit
+
+class OMDBImageDownloadOperation: NSOperation {
+
+}
